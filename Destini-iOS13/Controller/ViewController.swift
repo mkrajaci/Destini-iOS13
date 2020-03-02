15196900a470e646143f2bab3107bae2e653cbb0
Mario Krajacic <krajacic.mario@gmail.com>
2020-03-02 07:22:04 +0100

    Added struct for stories

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -15,17 +15,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice2Button: UIButton!
     @IBAction func choiceMade(_ sender: Any) {
     }
+    
     let story0 = "You see a fork in the road."
     let choice1 = "Take a left."
     let choice2 = "Take a right."
+    let stories = [
+        Story(title: "You see fork in a road.", choice1: "Take a left", choice2: "Take a right"),
+        Story(title: "You see a tigar.", choice1: "Shout for help", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps")
+    ]
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = 
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
-
+        storyLabel.text = stories[Story().storyNumber].title
+        choice1Button.setTitle(stories[Story().storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[Story().storyNumber].choice2, for: .normal)
     }
 
 
